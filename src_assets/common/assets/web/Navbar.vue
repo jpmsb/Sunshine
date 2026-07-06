f32960ccb4b375c6c2ffa5cfdc6cf42e2c2a7335
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2026-07-06 20:51:27 -0300

    :bug: fix(web): Remove navbar wrapper so sticky positioning works on scroll.

--- src_assets/common/assets/web/Navbar.vue
+++ src_assets/common/assets/web/Navbar.vue
@@ -1,90 +1,88 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-sunshine">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="./" title="Sunshine">
-          <img src="/images/logo-sunshine-45.png" height="45" alt="Sunshine">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="./">
-                <Home :size="18" class="icon"></Home>
-                {{ $t('navbar.home') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./pin">
-                <Lock :size="18" class="icon"></Lock>
-                {{ $t('navbar.pin') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./devices">
-                <Smartphone :size="18" class="icon"></Smartphone>
-                {{ $t('navbar.devices') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./apps">
-                <Layers :size="18" class="icon"></Layers>
-                {{ $t('navbar.applications') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./featured">
-                <Star :size="18" class="icon"></Star>
-                {{ $t('navbar.featured') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./config">
-                <Settings :size="18" class="icon"></Settings>
-                {{ $t('navbar.configuration') }}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./troubleshooting">
-                <Info :size="18" class="icon"></Info>
-                {{ $t('navbar.troubleshoot') }}
-              </a>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <ThemeToggle/>
-            </li>
-            <li class="nav-item dropdown">
-              <button class="nav-link dropdown-toggle" type="button" id="navbarUserMenu"
-                      data-bs-toggle="dropdown" aria-expanded="false" aria-label="User menu" title="User menu">
-                <CircleUserRound :size="18" class="icon"></CircleUserRound>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUserMenu">
-                <li>
-                  <a class="dropdown-item d-flex align-items-center" href="./password">
-                    <Shield :size="18" class="icon"></Shield>
-                    {{ $t('navbar.password') }}
-                  </a>
-                </li>
-                <li><hr class="dropdown-divider"></li>
-                <li>
-                  <button type="button" class="dropdown-item d-flex align-items-center" @click="logout">
-                    <LogOut :size="18" class="icon"></LogOut>
-                    {{ $t('navbar.logout') }}
-                  </button>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-sunshine">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./" title="Sunshine">
+        <img src="/images/logo-sunshine-45.png" height="45" alt="Sunshine">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="./">
+              <Home :size="18" class="icon"></Home>
+              {{ $t('navbar.home') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./pin">
+              <Lock :size="18" class="icon"></Lock>
+              {{ $t('navbar.pin') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./devices">
+              <Smartphone :size="18" class="icon"></Smartphone>
+              {{ $t('navbar.devices') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./apps">
+              <Layers :size="18" class="icon"></Layers>
+              {{ $t('navbar.applications') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./featured">
+              <Star :size="18" class="icon"></Star>
+              {{ $t('navbar.featured') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./config">
+              <Settings :size="18" class="icon"></Settings>
+              {{ $t('navbar.configuration') }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./troubleshooting">
+              <Info :size="18" class="icon"></Info>
+              {{ $t('navbar.troubleshoot') }}
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <ThemeToggle/>
+          </li>
+          <li class="nav-item dropdown">
+            <button class="nav-link dropdown-toggle" type="button" id="navbarUserMenu"
+                    data-bs-toggle="dropdown" aria-expanded="false" aria-label="User menu" title="User menu">
+              <CircleUserRound :size="18" class="icon"></CircleUserRound>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUserMenu">
+              <li>
+                <a class="dropdown-item d-flex align-items-center" href="./password">
+                  <Shield :size="18" class="icon"></Shield>
+                  {{ $t('navbar.password') }}
+                </a>
+              </li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <button type="button" class="dropdown-item d-flex align-items-center" @click="logout">
+                  <LogOut :size="18" class="icon"></LogOut>
+                  {{ $t('navbar.logout') }}
+                </button>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <Notification></Notification>
-  </div>
+    </div>
+  </nav>
+  <Notification></Notification>
 </template>
 
 <script>
